e6dc7939c5a89ebf7b3946f908df0f911e83b6fd
muladamai <muladamai@yahoo.com>
2015-10-13 10:35:09 +0700

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,16 @@
+<link rel="shortcut icon" type="image/x-icon" href="http://indoconx.com/wp-content/uploads/2015/02/favicon.png">
+<link rel="stylesheet" id="unite-bootstrap-css" href="http://indoconx.com/wp-content/themes/unite/inc/css/bootstrap.min.css?ver=4.3.1" type="text/css" media="all">
+
+<div class="container">
+<p>Thank you for your feedback</p>
+<button id="btnclose" type="button" class="close" data-dismiss="modal">close</button>
+</div>
+<script type="text/javascript">
+document.getElementById ("btnclose").onclick=function () {
+	console.log ("asd")
+  
+    window.parent.jQuery('#modalfeed').modal('hide');
+	console.log("qwe")
+
+};
+</script>
